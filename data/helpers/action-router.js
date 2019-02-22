baaef32dd612e93d10aa6291d7a8bd7d73052f9b
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-22 11:42:34 -0500

    get for actions functioning

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -2,3 +2,19 @@ const express = require('express');
 
 const  db = require('./actionModel.js')
 const actionRouter = express.Router();
+
+actionRouter.get('/', async (req, res) => {
+    try {
+      const actions = await db.get();   
+      res.status(200).json(actions);
+    } catch (error) {
+      res.status(500).json({
+        message: 'Error retrieving the actions',
+      });
+    }
+  });
+
+
+
+  module.exports = actionRouter;
+  
